0576a8bf782df520e56fd6d7993829a5e487f54e
Vojtěch Hutla <vojtech@VojtovoNTB.hutlovi.net>
2015-06-18 19:06:49 +0200

    2015-06-18c - Added file for Template engine and updated framework bootstraping

--- framework/evolve.php
+++ framework/evolve.php
@@ -18,27 +18,33 @@
 define('EVOLVE_VER','0.0.1_indev');
 
 /* Define constants */
-define('EVOLVE_DIR','./framework');
-define('EVOLVE_INDEV','false');
-define('EVOLVE_SETTINGS','./framework/settings/settings.json');
-define('EVOLVE_MODE','complete');
-
-/* If is it required by developer, rewrite selected constants */
-if(!empty($ecore_arg)) {
-	/* List of constants */
-	$ecore_constants = array(
-		'work_dir' => 'EVOLVE_DIR',
-		'devel_mode' => 'EVOLVE_INDEV',
-    'settings_file' => 'EVOLVE_SETTINGS',
-		'mode' => 'EVOLVE_MODE'
-	);
-
-	foreach($ecore_arg as $key => $value) {
-		if(isset($ecore_constants[$key])) {
-			define($ecore_constants[$key],$value);
-		}
+	/* Set working directory */
+	if(isset($ecore_arg['dir'])) {
+		define('EVOLVE_DIR',$ecore_arg['dir']);
+	} else {
+		define('EVOLVE_DIR','./framework');
 	}
-}
+
+	/* Set settings file path */
+	if(isset($ecore_arg['settings_path'])) {
+		define('EVOLVE_SETTINGS',$ecore_arg['settings_path']);
+	} else {
+		define('EVOLVE_SETTINGS','./framework/settings/settings.json');
+	}
+
+	/* Set framework mode */
+	if(isset($ecore_arg['mode'])) {
+		define('EVOLVE_MODE',$ecore_arg['mode']);
+	} else {
+		define('EVOLVE_MODE','complete');
+	}
+
+	/* Set development mode */
+	if(isset($ecore_arg['devmode'])) {
+		define('EVOLVE_INDEV',$ecore_arg['devmode']);
+	} else {
+		define('EVOLVE_INDEV','false');
+	}
 
 /* Manually include core components */
 require_once('core/Tracer.php');
@@ -46,12 +52,12 @@ require_once('core/CompatibilityChecker.php');
 require_once('core/Settings.php');
 require_once('core/Loader.php');
 
-if(constant('EVOLVE_MODE') == 'complete' OR constant('EVOLVE_MODE') == 'development') {
+if(EVOLVE_MODE == 'complete' OR EVOLVE_MODE == 'api_mode') {
 	require_once('core/Security.php');
   require_once('core/Router.php');
 
-	if(constant('EVOLVE_MODE') == 'development') {
-		echo 'Development tools: active';
+	if(EVOLVE_MODE == 'complete') {
+		require_once('core/Templates.php');
 	}
 }
 
--- index.php
+++ index.php
@@ -1,6 +1,33 @@
 <?php
+/**
+ * ███████╗██╗   ██╗ ██████╗ ██╗    ██╗   ██╗███████╗
+ * ██╔════╝██║   ██║██╔═══██╗██║    ██║   ██║██╔════╝
+ * █████╗  ██║   ██║██║   ██║██║    ██║   ██║█████╗
+ * ██╔══╝  ╚██╗ ██╔╝██║   ██║██║    ╚██╗ ██╔╝██╔══╝
+ * ███████╗ ╚████╔╝ ╚██████╔╝███████╗╚████╔╝ ███████╗
+ * ╚══════╝  ╚═══╝   ╚═════╝ ╚══════╝ ╚═══╝  ╚══════╝
+ *
+ * Evolve Framework
+ * Copyright (c) Evolve Team
+ *
+ * Licensed under MIT
+ * evolve.github.io
+ */
+
+/* Only in dev versions */
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
+/* ==================== */
+
+/* Start time counter */
+$start_time = microtime(true);
+
+/* Load application */
 include('framework/evolve.php');
 include('app/loader.php');
-echo 'Hi';
+
+/* Stop time counter */
+$timer = microtime(true)-$start_time;
+$timer = round($timer, 10);
+
+/* Initialite development features */
